802a23a7e523a344b72f11e7633f987c65fe3280
iftykhar alam <s.m.ifty49@gmail.com>
2026-04-19 00:43:19 +0600

    fix: fixing bugs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    resolveAlias: {
+      "tw-animate-css": "tw-animate-css/dist/tw-animate.css",
+    },
+  },
 };
 
 export default nextConfig;
